a708a392f4348406b861b10f24fe001271517641
Kevin Zheng <kevin.zhe72@gmail.com>
2017-02-15 17:29:43 -0800

    asdf

--- src/org/usfirst/frc/team2854/robot/commands/DriveCommand.java
+++ src/org/usfirst/frc/team2854/robot/commands/DriveCommand.java
@@ -1,24 +1,44 @@
 package org.usfirst.frc.team2854.robot.commands;
 
+import java.io.IOException;
+
 import org.usfirst.frc.team2854.robot.OI;
 import org.usfirst.frc.team2854.robot.subsystems.DriveTrain;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import Networking.*;
+import Visual.*;
 
 public class DriveCommand  extends Command{
 
 	private DriveTrain train;
 	private OI oi;
+
+	private Client client = new Client("192.168.50.233",44);
+	VisualData[] visualData;
 	
 	public DriveCommand(DriveTrain train, OI oi) {
 		super();
 		this.train = train;
 		this.oi = oi;
 	}
-	
+	@Override
+	protected void initialize(){
+	}
 	@Override
 	protected void execute() {
-		
+		try {
+			if(client.isNewData()) {
+				visualData = VisualData.decode(client.getLatest());
+			}
+			if(visualData != null) {
+				SmartDashboard.putString("visualData-idk", visualData[0].toString());
+			}
+			
+		} catch (ClassNotFoundException | IOException e) {
+			e.printStackTrace();
+		}
 		train.setPower(oi.controller0.ary.get(),-oi.controller0.aly.get());
 	//I know it doesn't make sense. Just for testing;
 	}
